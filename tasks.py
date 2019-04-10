33e0384c91da96ce11fb1af6ce87edee64e0e3eb
Chancy Kennedy <chancykennedy@gmail.com>
2019-04-09 21:25:39 -0500

    check task

--- tasks.py
+++ tasks.py
@@ -14,16 +14,23 @@ def execute_command(cmd_name, cmd):
         version = f'{v.major}.{v.minor}.{v.patch}'
         msg = f'[INFO] {cmd_name} complete: {version}'
         print('-' * len(msg))
-        print('\n' + msg + '\n')
+        print(msg + '\n')
     else:
         msg = f'[ERROR] {cmd_name} failed. See above.'
         print('-' * len(msg))
-        print('\n' + msg + '\n')
+        print(msg + '\n')
 
 
 @click.group()
 def tasks_cli():
     pass
+
+
+@click.command()
+def check():
+    execute_command('Flake8', ['flake8'])
+    execute_command('MyPy', ['mypy', 'fuzzyjoin'])
+    execute_command('PyTest', ['python', '-m', 'pytest', 'tests'])
 
 
 @click.command()
@@ -57,6 +64,7 @@ def publish(test):
     execute_command('Publish', cmd)
 
 
+tasks_cli.add_command(check)
 tasks_cli.add_command(build)
 tasks_cli.add_command(publish)
 
